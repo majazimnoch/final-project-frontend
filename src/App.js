c7bd1c03f185e9fdf0be57e56dbee597338144c1
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-05-29 20:41:33 +0200

    created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import Navbar from 'components/BeforeLogin/Navbar'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Navbar />
   )
 }
---
+++ src/components/BeforeLogin/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <p>sndjncjsc</p>
+  )
+};
+
+export default Navbar;
