e00b81f1bc1670e58916705f8556f890bbf0d47b
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-05-30 13:53:01 +0200

    FAQ-Accordion works, not styled 100% - need to ask team

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Navbar from 'components/BeforeLogin/Navbar'
+import { FaqAccordion } from 'components/BeforeLogin/FaqAccordion'
 
 export const App = () => {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <FaqAccordion />
+    </>
   )
 }
---
+++ src/components/BeforeLogin/FaqAccordion.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { accordionData } from './accordionData';
+import { Accordion, AccordionContent, AccordionTitle } from './Faq.style';
+
+// chatGPT suggested to rename this to Accordion instead for consistency but I didn't do that yet
+const FaqAccordionItem = ({ title, content }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <Accordion>
+      <AccordionTitle onClick={() => setIsActive(!isActive)}>
+        <div>{title}</div>
+        <div>{isActive ? '-' : '+'}</div>
+      </AccordionTitle>
+      {isActive && <AccordionContent>{content}</AccordionContent>}
+    </Accordion>
+  );
+};
+
+export const FaqAccordion = () => {
+  return (
+    <div>
+      {accordionData.map(({ title, content }) => (
+        <FaqAccordionItem key={uuidv4()} title={title} content={content} />
+      ))}
+    </div>
+  );
+};
+
