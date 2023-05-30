b0b748c67a14f7f136402ea676c0b79110cf0977
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-05-30 15:41:41 +0200

    built the responsive navbar without styling

--- src/components/BeforeLogin/Navbar.js
+++ src/components/BeforeLogin/Navbar.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle('responsive_nav');
+  };
+
   return (
-    <p>sndjncjsc</p>
+    <header>
+      <div>
+        <img className="logo-image" alt="Logo Horsey" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png" />
+      </div>
+      <nav ref={navRef}>
+        <a href="/faq">FAQ</a>
+        <a href="/conatct">Contact</a>
+        <button
+          type="button"
+          className="nav-btn nav-close-btn"
+          onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button
+        type="button"
+        className="nav-btn"
+        onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
   )
-};
+}
 
 export default Navbar;
