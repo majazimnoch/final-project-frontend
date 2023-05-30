b919189b1001793a485e408e61e0e020c7a7f227
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-05-30 20:50:42 +0200

    styling of the navbar with styled components, some parts are with css in the main css file

--- src/components/BeforeLogin/Navbar.js
+++ src/components/BeforeLogin/Navbar.js
@@ -1,5 +1,23 @@
 import React, { useRef } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import LogoIcon from 'assets/logo-icon.svg';
+import styled from 'styled-components';
+
+const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 80px;
+    padding: 0 0;
+    background-color: var(--claret);
+    color: var(--primaryWhite);
+
+    @media only screen and (max-width:1024px) {
+    }   
+`
+const Img = styled.img`
+  height: 80px;
+`
 
 const Navbar = () => {
   const navRef = useRef();
@@ -9,27 +27,29 @@ const Navbar = () => {
   };
 
   return (
-    <header>
+    <Header>
       <div>
-        <img className="logo-image" alt="Logo Horsey" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png" />
+        <Img alt="Logo Horsey" src={LogoIcon} />
       </div>
-      <nav ref={navRef}>
-        <a href="/faq">FAQ</a>
-        <a href="/conatct">Contact</a>
+      <div>
+        <nav ref={navRef}>
+          <a href="/faq">FAQ</a>
+          <a href="/conatct">Contact</a>
+          <button
+            type="button"
+            className="nav-btn nav-close-btn"
+            onClick={showNavbar}>
+            <FaTimes />
+          </button>
+        </nav>
         <button
           type="button"
-          className="nav-btn nav-close-btn"
+          className="nav-btn"
           onClick={showNavbar}>
-          <FaTimes />
+          <FaBars />
         </button>
-      </nav>
-      <button
-        type="button"
-        className="nav-btn"
-        onClick={showNavbar}>
-        <FaBars />
-      </button>
-    </header>
+      </div>
+    </Header>
   )
 }
 
