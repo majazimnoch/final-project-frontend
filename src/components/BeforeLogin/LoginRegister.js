32cc6fab11dd20cd05cbf38200d7e217793ad3a0
SandraMadeleine <sandramohlsson@yahoo.com>
2023-06-06 22:14:00 +0200

    adding code in LoginRegister

--- src/components/BeforeLogin/LoginRegister.js
+++ src/components/BeforeLogin/LoginRegister.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Button } from './Button';
 
-const HeroAction = () => {
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const LoginRegister = ({
+  state,
+  name,
+  setName,
+  email,
+  setEmail,
+  password,
+  setPassword,
+  handleFormSubmit
+}) => {
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
-    <p>Hej</p>
-  )
+    <StyledForm
+      onSubmit={(event) => {
+        event.preventDefault();
+        handleFormSubmit(event, state);
+      }}>
+
+      {state === 'register' && (
+        <input
+          type="text"
+          name="name"
+          value={name}
+          placeholder="enter your name"
+          onChange={handleNameChange} />
+      )}
+      <input
+        type="email"
+        name="email"
+        value={email}
+        placeholder="enter your email"
+        onChange={handleEmailChange} />
+
+      <input
+        type="password"
+        name="password"
+        value={password}
+        placeholder="enter your password"
+        onChange={handlePasswordChange} />
+      <Button
+        type="submit"
+        text={state === 'login' ? 'Login!' : 'Register!'}
+        variant={state === 'login' ? 'confirm' : 'warning'} />
+    </StyledForm>
+  );
 };
 
-export default HeroAction;
+export default LoginRegister;
