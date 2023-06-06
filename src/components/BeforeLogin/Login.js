aed0b099b7c540f9403cc175dec8a2d5b9141530
SandraMadeleine <sandramohlsson@yahoo.com>
2023-06-06 22:53:59 +0200

    added code to Login

---
+++ src/components/BeforeLogin/Login.js
@@ -0,0 +1,121 @@
+/* eslint-disable no-unused-expressions */
+import React, { useState } from 'react';
+import LoginRegister from './LoginRegister';
+import Notification from '../lib/Notification';
+
+const Login = ({ API_URL }) => {
+  const [loginOrRegister, setLoginOrRegister] = useState('login');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [notification, setNotification] = useState({});
+
+  const handleFormSubmit = (event, state) => {
+    switch (state) {
+      case 'login':
+        fetch(`${API_URL}/login`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ name, password })
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.success) {
+              localStorage.setItem('token', data.response.accessToken, {
+                path: '/'
+              });
+              window.location.href = '/';
+            } else {
+              setNotification({
+                message: 'error logging in',
+                variant: 'error'
+              });
+            }
+          })
+          .catch((error) => {
+            setNotification({
+              message: error,
+              variant: 'error'
+            });
+          });
+
+        break;
+      case 'register':
+        fetch(`${API_URL}/register`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ name, email, password })
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.success) {
+              localStorage.setItem('token', data.response.accessToken, {
+                path: '/'
+              });
+              window.location.href = '/';
+            } else {
+              setNotification({
+                message: 'error registering',
+                variant: 'error'
+              });
+            }
+          })
+          .catch((error) => {
+            setNotification({
+              message: error,
+              variant: 'error'
+            });
+          });
+        break;
+      default:
+        setNotification({
+          message: 'Invalid state',
+          variant: 'error'
+        });
+    }
+  };
+
+  const handleLoginOrRegister = () => {
+    loginOrRegister === 'login'
+      ? setLoginOrRegister('register')
+      : setLoginOrRegister('login');
+  };
+
+  return (
+    <>
+      {notification && (
+        <Notification
+          variant={notification.variant}
+          message={notification.message} />
+      )}
+      {/* <h1>Please log in first:</h1> */}
+      <LoginRegister
+        state={loginOrRegister}
+        name={name}
+        setName={setName}
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleFormSubmit={handleFormSubmit} />
+
+      {loginOrRegister === 'login' ? (
+        <p>
+          No account?{' '}
+          <a href="#" onClick={handleLoginOrRegister}>
+            Register!
+          </a>
+        </p>
+      ) : (
+        <p>
+          Already registered?{' '}
+          <a href="#" onClick={handleLoginOrRegister}>
+            Login!
+          </a>
+        </p>
+      )}
+    </>
+  );
+};
+
+export default Login;
