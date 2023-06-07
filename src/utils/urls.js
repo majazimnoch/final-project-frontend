1d300de3af3bc95cfc2659cc9d1f85246d33dde4
SandraMadeleine <sandramohlsson@yahoo.com>
2023-06-07 22:03:32 +0200

    final commit on the branch

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,5 @@
-// const BASE_URL = 'https://final-project-backend-q7mqhxeq3q-lz.a.run.app/';
-// export const API_URL = (slug) => `${BASE_URL}/${slug}`
+const BASE_URL = 'https://final-project-backend-q7mqhxeq3q-lz.a.run.app/';
+export const API_URL = (slug) => `${BASE_URL}/${slug}`
 
-const API_URL = 'https://final-project-backend-q7mqhxeq3q-lz.a.run.app/';
-
-export default API_URL;
+// const API_URL = 'https://final-project-backend-q7mqhxeq3q-lz.a.run.app/';
+// export default API_URL;
