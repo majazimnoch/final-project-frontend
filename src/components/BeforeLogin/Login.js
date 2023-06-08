00872eb205871ae1afdfa55303216f593611ec98
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-06-08 14:24:17 +0200

    styling of the log in page

--- src/components/BeforeLogin/Login.js
+++ src/components/BeforeLogin/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import LoginRegister from './LoginRegister';
 import Profile from './Startpage';
-// import
+import Horseplayful from '../../assets/horseplayful.svg'
 
 const token = localStorage.getItem('token');
 const API_URL = process.env.API_URL || 'https://final-project-backend-q7mqhxeq3q-lz.a.run.app/';
@@ -62,55 +62,107 @@ const Home = () => {
     : setLoginOrRegister('login'));
 
   return (
-    <StyledHome>
-      {token ? (
-        <Profile API_URL={API_URL} />
-      ) : (
-        <>
-          {loginOrRegister === 'login' ? (
-            <><h1>Welcome back, friend!</h1><p>Sign in and keep track of your horses!</p></>
-          ) : (
-            <><h1>New user? Welcome!</h1><p>Create a user account in 5 seconds!</p>
-            </>
-          )}
-          <LoginRegister
-            state={loginOrRegister}
-            name={name}
-            setName={setName}
-            email={email}
-            setEmail={setEmail}
-            password={password}
-            setPassword={setPassword}
-            handleFormSubmit={handleFormSubmit} />
-          {loginOrRegister === 'login' ? (
-            <p>
-              <a href="#" onClick={handleLoginOrRegister}>
-              I do not have an account yet!
-              </a>
-            </p>
-          ) : (
-            <p>
-              <a href="#" onClick={handleLoginOrRegister}>
-              I already have an account!
-              </a>
-            </p>
-          )}
-        </>
-      )}
-    </StyledHome>
+    <LoginWrapper>
+      <LeftLogin>
+        <TextLoginBox>
+          <StyledPlayfulHorse src={Horseplayful} alt="Little horse" />
+          <h2>Giddy up and get ready for the horse ride of your life!
+          </h2>
+          <p>Horsey app helps you track your progress, set goals, and stay motivated.
+             Sign up now and see what all the neighing is about!
+          </p>
+        </TextLoginBox>
+      </LeftLogin>
+      <RightLogin>
+        {token ? (
+          <Profile API_URL={API_URL} />
+        ) : (
+          <>
+            {loginOrRegister === 'login' ? (
+              <>
+                <WelcomeHeader>Welcome back, friend!</WelcomeHeader>
+                <p>Sign in and keep track of your horses!</p>
+              </>
+            ) : (
+              <>
+                <WelcomeHeader>New user? Welcome!</WelcomeHeader>
+                <p>Create a user account in 5 seconds!</p>
+              </>
+            )}
+            <LoginRegister
+              state={loginOrRegister}
+              name={name}
+              setName={setName}
+              email={email}
+              setEmail={setEmail}
+              password={password}
+              setPassword={setPassword}
+              handleFormSubmit={handleFormSubmit} />
+            {loginOrRegister === 'login' ? (
+              <p>
+                <a href="#" onClick={handleLoginOrRegister}>
+                  I do not have an account yet!
+                </a>
+              </p>
+            ) : (
+              <p>
+                <a href="#" onClick={handleLoginOrRegister}>
+                  I already have an account!
+                </a>
+              </p>
+            )}
+          </>
+        )}
+      </RightLogin>
+    </LoginWrapper>
   );
 };
 
 export default Home;
 
-const StyledHome = styled.div`
+const StyledPlayfulHorse = styled.img`
+  height: 100px;
+`
+
+const WelcomeHeader = styled.h2`
+  color: var(--primaryBlack);
+  font-size: 40px;
+
+  @media (max-width: 668px) {
+
+  }
+
+  @media (max-width: 1023px) {
+
+  }
+`
+
+const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 4rem;
+`
+
+const TextLoginBox = styled.div`
+  border: 2px purple solid;
+`
+
+const LeftLogin = styled.div`
+display: flex;
+border: 1px red solid;
+align-items: center;
+`
+
+const RightLogin = styled.div`
   display: flex;
   flex-direction: column;
   align-content: flex-start;
-  min-height: 100vh;
-  width: 100vw;
   color: var(--primaryBlack);
-  background-color: lightblue;
+  border: 1px solid var(--softPurple);
+  border-radius: 50px;
+  padding: 3rem;
+  width: 50%;
+
 
   h1 {
     font-size: 4rem;
--- src/components/BeforeLogin/LoginRegister.js
+++ src/components/BeforeLogin/LoginRegister.js
@@ -1,6 +1,7 @@
 // import React, { useState } from 'react';
 import React from 'react';
 import styled from 'styled-components';
+import { StyledButton } from './ButtonStyling';
 // import Button from './Button';
 
 // eslint-disable-next-line max-len
@@ -57,11 +58,6 @@ export default LoginRegister;
 const Form = styled.form`
 display: flex;
 flex-direction: column;
-align-items: center;
-justify-content: center;
-height: 50vh;
-width: 100vw;
-margin: 0;
 background-color: var(--primaryWhite);
 `;
 
@@ -70,35 +66,12 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 gap: 2rem;
-
-@media (max-width: 720px) {
-  flex-direction: column;
-  }
-`;
-
-const StyledButton = styled.button`
-border-radius: 5px;
-border: 1px solid #ffffff;
-cursor: pointer;
-width: 7rem;
-background-color: var(--softPurple);
-color: #ffffff;
-font-size: 1rem;
-font-weight: bold;
-margin-top: 1.5rem;
-padding: 0.8rem;
-
-@media (max-width: 664px) {
-  border-radius: 10px;
-  padding: 0.3rem;
-  }
 `;
 
 const StyledInput = styled.input`
 height: 3rem;
-border-radius: 5px;
+border-radius: 10px;
 font-size: 1rem;
 font-weight: bold;
-margin-left: 1rem;
-color: #76b041;
+
 `;
