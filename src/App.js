55eb9b245f0a49158acb2f1291657a57f219b5f2
SandraMadeleine <sandramohlsson@yahoo.com>
2023-06-10 01:19:09 +0200

    paths work, yay!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import PublicMain from 'components/BeforeLogin/PublicMain';
+import LandingPage from './components/Pages/LandingPage';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<PublicMain />} />
+      <Route path="/landingpage" element={<LandingPage />} />
     </Routes>
   )
 }
