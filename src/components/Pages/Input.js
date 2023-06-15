0a584ab93bf346755118caa3b1be64588e7a743e
SandraMadeleine <sandramohlsson@yahoo.com>
2023-06-15 15:54:31 +0200

    updating components with styling and minor fixes

--- src/components/Pages/Input.js
+++ src/components/Pages/Input.js
@@ -1,16 +1,29 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+  height: 3rem;
+  padding: 0 7rem;
+  border-radius: 10px;
+  border: 1px var(--primaryBlack) solid;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+
+  &:focus {
+    outline: 0.5px solid var(--softPurple);
+  }
+`;
 
 const Input = ({ type, placeholder, value, onChange }) => {
   return (
     <label>
-      <input
+      <StyledInput
         type={type}
         placeholder={placeholder}
         value={value}
         onChange={onChange} />
     </label>
-  )
-}
+  );
+};
 
 export default Input;
--- src/components/Pages/InputTextArea.js
+++ src/components/Pages/InputTextArea.js
@@ -1,15 +1,30 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledTextArea = styled.textarea`
+  height: 150px;
+  padding: 0.5rem;
+  border-radius: 10px;
+  border: 1px solid var(--primaryBlack);
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+  max-width: 200px;
+  max-height:200px;
+
+  &:focus {
+    outline: 0.5px solid var(--softPurple);
+  }
+`;
 
 const InputTextArea = ({ placeholder, value, onChange }) => {
   return (
     <label>
-      <textarea
+      <StyledTextArea
         placeholder={placeholder}
         value={value}
         onChange={onChange} />
     </label>
-  )
-}
+  );
+};
 
 export default InputTextArea;
