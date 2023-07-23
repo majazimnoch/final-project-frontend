b0c652185b8149ed123c081f5dd7662d3ca2e144
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-07-23 16:15:45 +0200

    added api with pictures of horses, using react hooks

--- src/components/Pages/HorseDetails.js
+++ src/components/Pages/HorseDetails.js
@@ -1,11 +1,27 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { v4 as uuidv4 } from 'uuid';
 
 const HorseDetails = ({ horseInfo }) => {
   console.log(horseInfo);
+
+  // useState to handle the image URL state
+  const [horseImage, setHorseImage] = useState('');
+
+  // Fetch the image URL from the API
+  useEffect(() => {
+    fetch('https://source.unsplash.com/500x500/?horses')
+      .then((response) => {
+        // Get the URL of the fetched image
+        setHorseImage(response.url);
+      })
+      .catch((error) => {
+        console.error('Error fetching horse image:', error);
+      });
+  }, []);
+
   // Turning characteristics string into array (to be able to display the characteristics in a list). The string is split at each comma.
   const CharacteristicsIntoList = horseInfo.map((horse) => {
     return (
@@ -48,6 +64,7 @@ const HorseDetails = ({ horseInfo }) => {
                     <li key={uuidv4()}>{li}</li>
                   ))}
                 </ol>
+                {horseImage && <img src={horseImage} alt="Horse" />}
               </Instructions>
             </HorseInstructionsDiv>
           </MainHorseDiv>
